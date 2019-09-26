7bca971cb2633e30ee6a5f1f9c2c096fa35a3dac
Heorhii Siburov <g.siburov@gmail.com>
2019-09-25 23:50:43 -0700

    Darkmode options added

--- african-marketplace/src/App.js
+++ african-marketplace/src/App.js
@@ -9,16 +9,31 @@ import LoginForm from './components/Login';
 import Signup from './components/Signup';
 import Sell from './components/Sell';
 import AddProduct from './components/AddProduct';
+import useDarkMode from "./hooks/useDarkMode";
 
 function App() {
+
+  const [darkMode, setDarkMode] = useDarkMode();
+  console.log("Your mode is dark: ", darkMode)
+
+  const toggleMode = e => {
+    e.preventDefault();
+    setDarkMode(!darkMode);
+  };
 
   return (
     <Router>
       <div className="App">
-                <header className="App-header">
+        <header className="App-header">
           <h1>
             African Marketplace App
           </h1>
+            <div className="dark-mode__toggle">
+              <div
+              onClick={toggleMode}
+              className={darkMode ? 'toggle toggled' : 'toggle'}
+              />
+            </div>
         </header>
         <Route exact path="/" component={LoginForm} />
         <Route path="/signup" component={Signup} />
