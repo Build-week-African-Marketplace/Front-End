26f6841c12273bb10be0157f98c73e1cfc91f052
Heorhii Siburov <g.siburov@gmail.com>
2019-09-26 15:36:35 -0700

    works on fixing signup

--- african-marketplace/src/App.js
+++ african-marketplace/src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Dashboard from './components/Dashboard';
 import PrivateRoute from './components/PrivateRoute';
 import LoginForm from './components/Loginform';
-import Signup from './components/Signup';
+import SignupForm from './components/SingupForm';
 import Sell from './components/Sell';
 import AddProduct from './components/AddProduct';
 import useDarkMode from "./hooks/useDarkMode";
@@ -14,7 +14,7 @@ import useDarkMode from "./hooks/useDarkMode";
 function App() {
 
   const [darkMode, setDarkMode] = useDarkMode();
-  console.log("Your mode is dark: ", darkMode)
+  // console.log("Your mode is dark: ", darkMode)
 
   const toggleMode = e => {
     e.preventDefault();
@@ -26,7 +26,7 @@ function App() {
       <div className="App">
 
         <Route exact path="/" component={LoginForm} />
-        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/signup" component={SignupForm} />
         <PrivateRoute toggleMode={toggleMode} darkMode = {darkMode} path="/dashboard" component={Dashboard} />
         <PrivateRoute exact path="/sell" component={Sell} />
         <PrivateRoute exact path="/myads" component={AddProduct} />
--- african-marketplace/src/components/OutHeader.js
+++ african-marketplace/src/components/OutHeader.js
@@ -4,13 +4,7 @@ import useDarkMode from "../hooks/useDarkMode";
 
 
 function OutHeader() {
-  const logout = () => {
-    window.localStorage.removeItem("token");
-    window.localStorage.removeItem('userID');
-  };
-
   const [darkMode, setDarkMode] = useDarkMode();
-  console.log("Your mode is dark: ", darkMode)
 
   const toggleMode = e => {
     e.preventDefault();
